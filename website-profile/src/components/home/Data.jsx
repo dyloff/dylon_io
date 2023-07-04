a1a9b80a402b6fbd812bb121fc314e44bc1e9f7b
Dylon M <dylonmura@gmail.com>
2023-07-04 17:17:58 +0100

    profile pic animation

--- website-profile/src/components/home/Data.jsx
+++ website-profile/src/components/home/Data.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Data = () => {
   return (
     <div className="home__data">
-      <h1 className="home__title">Dylon M
+      <h1 className="home__title">Dylon Marwaha
       <svg
                   width="36"
                   height="36"
