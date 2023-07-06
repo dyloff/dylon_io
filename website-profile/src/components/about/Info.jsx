565a006d040be986580accab83d3ccb3d52801e0
Dylon M <dylonmura@gmail.com>
2023-07-06 11:35:30 +0100

    about me section complete + added responsive screen size

--- website-profile/src/components/about/Info.jsx
+++ website-profile/src/components/about/Info.jsx
@@ -8,14 +8,14 @@ const info = () => {
         <i class='bx bx-briefcase-alt about__icon'></i>
 
         <h3 className="about__title">Interests</h3>
-        <span className="about__subtitle">FinTech, HealthTech, PropTech</span>
+        <span className="about__subtitle">FinTech Health/PropTech</span>
       </div>
 
       <div className="about__box">
         <i class='bx bx-cross about__icon' ></i>
 
-        <h3 className="about__title">LF Opportunities</h3>
-        <span className="about__subtitle">Full time - Remote or Office</span>
+        <h3 className="about__title">LF Open Roles</h3>
+        <span className="about__subtitle">Full time only Remote or Office</span>
       </div>
 
       <div className="about__box">
