53627ab0eaa4d9296df54a4a09c825a056f89583
Dylon <dm@Dylons-MBP.cust.communityfibre.co.uk>
2023-09-26 12:22:05 +0100

    minor fix 2

--- src/components/home/Data.jsx
+++ src/components/home/Data.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Data = () => {
   return (
     <div className="home__data">
-      <h1 className="home__title">Dylon Marwaha 👋
+      <h1 className="home__title">Dylon M 👋
       {/* <svg
                   width="36"
                   height="36"
